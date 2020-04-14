ea68c7bef0798e670ed0a6f5c22b3bc2769f7c2d
Marine M <marine.michenaud@edu.ece.fr>
2020-04-14 11:53:40 +0200

    Ajout Ferraille

--- Front/Ferraille.php
+++ Front/Ferraille.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="Acceuil.css">
+  <link rel="stylesheet" type="text/css" href="Ferraille.css">
   <link href="Images/Logo_EbayECE.ico" rel="icon" type="images/x-icon">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
@@ -19,43 +19,43 @@
 ?>
 
 <div class="container text-center">  
-  <br><h2>Articles du jour</h2><br>
+  <br><h2>Catégorie : Ferraille et Trésor</h2><br>
 
   <div class="col-xs-6 col-md-4">
     <div class="thumbnail">
-      <a href="CavalierBronze.jpg" title="">
-          <img src="Images/CavalierBronze.jpg" class="thumbnail" alt="Cavalier" height="250" width="140">
+      <a href="Images/Pieces1.png" title="">
+          <img src="Images/Pieces1.png" class="thumbnail" alt="Pieces1">
       </a>
       <div class="caption">
-         <p>Pièce Unique - Grande Cavalière Africaine en Bronze - Burkina Faso - Signe Daouda Derme</p>
+         <p>Lot de 6 pièces de 20 francs en or coq de Chaplain de 1907 et une 1912</p>
          <p>Catégorie : Ferraille ou Trésor</p>
-         <p>Prix : 210 €</p>
+         <p>Prix : 1 500 €</p>
      </div>
     </div>
   </div>
 
   <div class="col-xs-6 col-md-4">
     <div class="thumbnail">
-      <a href="VaseAsie.jpg" title="">
-          <img src="Images/VaseAsie.jpg" class="thumbnail" alt="Vase1" height="230" width="135">
+      <a href="Images/Pieces2.1.png" title="">
+          <img src="Images/Pieces2.1.png" class="thumbnail" alt="Pieces2">
       </a>
       <div class="caption">
-         <p>Petit vase en cuivre et émaux cloisonnés, Japon, époque Meiji, fin du XIXe siècle</p>
-         <p>Catégorie : Bon pour le Musée</p>
-         <p>Prix : 195 €</p>
+         <p>Médaille circulaire en bronze figurant Raspail "L'ami du peuple" de profil. Diamètre : 14 cm.</p>
+         <p>Catégorie : Ferraille ou Trésor</p>
+         <p>Prix : 15 €</p>
      </div>
     </div>
   </div>
 
   <div class="col-xs-6 col-md-4">
     <div class="thumbnail">
-      <a href="tableau1.jpg" title="">
-          <img src="Images/tableau1.jpg" class="thumbnail" alt="Tableau1" height="550" width="300">
+      <a href="Images/Pieces3.1.png" title="">
+          <img src="Images/Pieces3.1.png" class="thumbnail" alt="Pieces3">
       </a>
       <div class="caption">
-         <p>Très belle toile "Le coin du Parc Monceau", 45x55cm, signé Charles Angrand, époque du pointillisme.</p>
-         <p>Catégorie : Bon pour le Musée</p>
-         <p>Prix : 1 820 €</p>
+         <p>Lot de 3 pièce de 20 francs en or Napoléon III dattant de 1853, 1860 et 1862</p>
+         <p>Catégorie : Ferraille ou TRésor</p>
+         <p>Prix : 550 €</p>
      </div>
     </div>
   </div>
