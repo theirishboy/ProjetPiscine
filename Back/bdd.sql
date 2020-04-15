8f7b3e1335026ed594b5dd3b745c67c18022018f
PierreG <gabriel5112@hotmail.fr>
2020-04-15 17:09:45 +0200

    a

--- Back/bdd.sql
+++ Back/bdd.sql
@@ -9,8 +9,8 @@ CREATE TABLE `ebayece`.`Vendeur` ( `Humain` INT(10) NOT NULL , INDEX (`Humain`))
 CREATE TABLE `ebayece`.`Admin` ( `Humain` INT(10) NOT NULL , INDEX (`Humain`)) ENGINE = MyISAM; 
 CREATE TABLE `ebayece`.`objet art` ( `ID` INT(10) NOT NULL , `Nom` VARCHAR(50) NOT NULL , `Description` VARCHAR(250) NOT NULL , `Categorie` VARCHAR(50) NOT NULL , `Type de vente` VARCHAR(50) NOT NULL , `Prix` INT(10) NOT NULL ,`Cheminimg` VARCHAR(250) NOT NULL,PRIMARY KEY (`ID`)) ENGINE = MyISAM; 
 CREATE TABLE `ebayece`.`client` ( `Humain` INT(10) NOT NULL , `N_Telephone` VARCHAR(10) NOT NULL ,`Pays` VARCHAR(50) NOT NULL , `Ville` VARCHAR(50) NOT NULL , `Code_Postal` VARCHAR(50) NOT NULL , `AdresseL1` VARCHAR(50) NOT NULL , `AdresseL2` VARCHAR(50) NOT NULL , `Type_de_carte` VARCHAR(50) NOT NULL , `Numero_de_carte` VARCHAR(20) NOT NULL,`Nomcarte` VARCHAR(50) NOT NULL,`Dateexpi` DATE NOT NULL,`Codesecu` VARCHAR(5) NOT NULL , INDEX (`Humain`)) ENGINE = MyISAM; 
-CREATE TABLE `ebayece`.`panier` ( `Humain` INT(10) NOT NULL , `Objet` INT(10) NOT NULL , `Prix` VARCHAR(10) NOT NULL , INDEX `Humain` (`Humain`), INDEX `Objetd'art` (`Objet`)) ENGINE = MyISAM; 
-CREATE TABLE `ebayece`.`objetvendu` ( `Objet` INT(10) NOT NULL , `Humain` INT(10) NOT NULL , `Prix` VARCHAR(10) NOT NULL , INDEX `Objet d'art` (`Objet`), INDEX `Humain` (`Humain`)) ENGINE = MyISAM; 
+CREATE TABLE `ebayece`.`panier` ( `Humain` INT(10) NOT NULL , `Objet` INT(10) NOT NULL , `Prix` VARCHAR(10) NOT NULL , INDEX `Humain` (`Humain`), INDEX `Objet art` (`Objet`)) ENGINE = MyISAM; 
+CREATE TABLE `ebayece`.`objetvendu` ( `Objet` INT(10) NOT NULL , `Humain` INT(10) NOT NULL , `Prix` VARCHAR(10) NOT NULL , INDEX `Objet art` (`Objet`), INDEX `Humain` (`Humain`)) ENGINE = MyISAM; 
 
 INSERT INTO `ebayece`.`humain` (`ID`, `Nom`, `Prenom`, `Login`, `Mot_de_passe`, `Mail`, `Media`) VALUES ('1', 'Gabriel', 'Pierre', 'leroux', PASSWORD('motdepasse'),  'pierre.gabriel@edu.ece.fr', '1');
 INSERT INTO `ebayece`.`humain` (`ID`, `Nom`, `Prenom`, `Login`, `Mot_de_passe`, `Mail`, `Media`) VALUES ('2', 'Michenaud ', 'Marine', 'MarineMch', PASSWORD('motdepasse'),  'marine.michenaud@edu.ece.fr', '2');
