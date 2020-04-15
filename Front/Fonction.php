efbcbf001168557af3213aa1ce4f153359a4f012
Marine M <marine.michenaud@edu.ece.fr>
2020-04-15 17:19:42 +0200

    Modif Ferraille.php + Fonction.php

--- Front/Fonction.php
+++ Front/Fonction.php
@@ -42,6 +42,7 @@ function listobjet($categorie)
 	}
 }
 
+
 function objetsvendus($ID)
 {
 	$result = connection("SELECT * FROM 'objetvendu' WHERE 'objetvendu'.'Humain' = '$ID'");
@@ -58,6 +59,45 @@ function objetsvendus($ID)
 		echo ' </div>';
 		echo ' </div>';
 		echo ' </div>';
+		
+	$result = connection("SELECT * FROM `objet art` WHERE `objet art`.`categorie` = '$categorie'");
+	$a = 0;//nb de colonne
+	$b=0;
+	while($data = mysqli_fetch_assoc($result))
+	{
+		if($a % 3 == 0)
+		{
+			echo '<div class="row">';
+		}
+		echo '<div class="col-xs-6 col-md-4">';
+		echo '<div class="thumbnail">';
+		
+		echo '<img src="'.$data['Cheminimg'].'" class="thumbnail" alt="Pieces1">';
+
+		echo '<div class="caption">';
+		echo '<p>'.$data['Nom'].'</p>';
+		echo '<p>'.$data['Description'].'</p>';
+		echo '<p>Catégorie : '.$data['Categorie'].'</p>';
+		echo ' <p>Prix : '.$data['Prix'].'€</p><br>';
+		echo '<a href="Itemessai.php?ID='.$data['ID'].'" id="VoirItem">Voir Item</a>';
+		echo ' <br><br></div>';
+		echo ' </div>';
+		echo ' </div>';
+		$b = $a+1;
+		if($a % 3 == 2)
+		{
+			echo '</div>';
+		}
+
+		if($b== mysqli_num_rows($result)){
+			if($a % 3 == 0 || $a % 3 == 1){
+			for($i = 0; $i<3-($a % 3); $i++){
+				echo '</div class="thumbnail">';
+			}
+		}
+		}
+
+		$a+=1;
 	}
 }
 
