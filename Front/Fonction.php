d150ea96c37fe7aea0911d9df6feaa3736db1971
PierreG <gabriel5112@hotmail.fr>
2020-04-15 16:33:06 +0200

    Remisefon

--- Front/Fonction.php
+++ Front/Fonction.php
@@ -1,26 +1,50 @@
 <?php 
-function listobjet($categorie)
-{
-	$result = connection("SELECT * FROM `objet d'art` WHERE `objet d'art`.`categorie` = '$categorie'");
-	//$data = mysqli_fetch_assoc($result);
-	while($data = mysqli_fetch_assoc($result))
-	{
-		
-		echo '<div class="col-xs-6 col-md-4">';
-		echo '<div class="thumbnail">';
-		echo '<a href="Images/Pieces1.png" title="">';
-		echo '<img src="'.$data['Cheminimg'].'" class="thumbnail" alt="Pieces1">';
+<?php 
+function listobjet($categorie) 
+{ 
+	$result = connection("SELECT * FROM `objet d'art` WHERE `objet d'art`.`categorie` = '$categorie'"); 
+	$a = 0;//nb de colonne 
+	$b=0; 
+	while($data = mysqli_fetch_assoc($result)) 
+	{ 
+		 
+		if($a % 3 == 0) 
+		{ 
+			echo '<div class="row">'; 
+		} 
+		echo '<div class="col-xs-6 col-md-4">'; 
+		echo '<div class="thumbnail">'; 
+		 
+		echo '<img src="'.$data['Cheminimg'].'" class="thumbnail" alt="Pieces1">'; 
+ 
+		echo '<div class="caption">'; 
+		echo '<p>'.$data['Nom'].'</p>'; 
+		echo '<p>'.$data['Description'].'</p>'; 
+		echo '<p>Catégorie : '.$data['Categorie'].'</p>'; 
+		echo ' <p>Prix : '.$data['Prix'].'€</p>'; 
+		echo ' </div>'; 
+		echo ' </div>'; 
+		echo ' </div>'; 
+ 
+		$b = $a+1; 
+		if($a % 3 == 2) 
+		{ 
+			echo '</div>'; 
+		} 
+ 
+		if($b== mysqli_num_rows($result)){ 
+			if($a % 3 == 0 || $a % 3 == 1){ 
+			for($i = 0; $i<3-($a % 3); $i++){ 
+				echo '</div class="thumbnail">'; 
+			} 
+		} 
+		} 
+ 
+		$a+=1; 
+	} 
+} 
 
-		echo '<div class="caption">';
-		echo '<p>'.$data['Nom'].'</p>';
-		echo '<p>'.$data['Description'].'</p>';
-		echo '<p>Catégorie : '.$data['Categorie'].'</p>';
-		echo ' <p>Prix : '.$data['Prix'].'€</p>';
-		echo ' </div>';
-		echo ' </div>';
-		echo ' </div>';
-	}
-}
+?>
 
 function objetsvendus($ID)
 {
