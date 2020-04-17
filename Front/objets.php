2a7e5fbe3328bf98b6d4148df5ea01e177941a82
Rayan Perrin <rayan.perrin@edu.ece.fr>
2020-04-17 10:05:02 +0200

    MAJ Front Vendeur

--- Front/objets.php
+++ Front/objets.php
@@ -13,43 +13,26 @@
 </head>
 <body>
 
-<?php
-    include("nav.php");
-?>
+    <?php
+        include("nav.php");
+    ?>
 
-<h1 style="text-align: center">Liste de vos objets vendus</h1>
+    <h1 style="text-align: center">Liste de vos objets vendus</h1>
+    <br>
+    <h2>Bon Pour le musée</h2>
+    <div class="object-card">
+        <img src="Images/tableau2.jpg" alt="tableau" width="160px">
+        <div class="description-card">
+            <h3>Le port de Collioure</h3>
+            <h5>Henri MARTIN (1860 - 1943)</h5>
+            <h6>Prix : 70 000€</h6>
+            <h6>Type de vente : Enchère</h6>
+        </div>
+    </div>
 
-<br>
-
-<div class="container">
-  <div class="row">
-    <div class="col-xs-4">
-    	<h2>Bon pour le musée</h2>
-    	<br>
-		<div style="width: 800px; height: 150px;  padding-top:10px; padding-bottom:10px;border: 3px solid #A0A0A0; text-align: center;">
-		<img src="Images/tableau2.jpg" class="display" alt="tableau" height="100px" width="100px">
-		<div class="caption">
-		<p>Henri MARTIN (1860 - 1943) <br> Le port de Collioure <br> Huile sur panneau 65 x 55 cm <br> Signé en bas à gauche Henri Martin 
-		<br><br>70 000€</p></div>
-		</div>
-		<br>
-		<div style="width: 800px; height: 150px;  padding-top:10px; padding-bottom:10px;border: 3px solid #A0A0A0; text-align: center;">
-		<img src="Images/tableau3.jpg" class="display" alt="tableau" height="100px" width="100px"> 
-		<div class="caption">
-		<p>Claude MONET (1840 - 1926) <br> Coucher de soleil sur la mer <br> Pastel 25 x 25 cm <br> Signé en bas à droite Claude Monet 
-		<br><br>200 000€</p></div>
-		</div>
-
-	</div>
-  </div>
-</div>
-
-
-<br><br>
-
-<?php
-    include("footer.php");
-?>
+    <?php
+        include("footer.php");
+    ?>
 
 </body>
 </html>
